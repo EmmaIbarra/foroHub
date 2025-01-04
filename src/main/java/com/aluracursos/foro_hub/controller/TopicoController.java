fc266d5ad3144696cf139a078abe421e5d878d5f
Emma Ibarra <eibarram.77@gmail.com>
2025-01-03 23:05:38 -0300

    Creación de controller de topico y adición método registro

---
+++ src/main/java/com/aluracursos/foro_hub/controller/TopicoController.java
@@ -0,0 +1,44 @@
+package com.aluracursos.foro_hub.controller;
+
+import com.aluracursos.foro_hub.domain.topico.DatosRegistroTopico;
+import com.aluracursos.foro_hub.domain.topico.DatosRespuestaTopico;
+import com.aluracursos.foro_hub.domain.topico.Topico;
+import com.aluracursos.foro_hub.domain.topico.TopicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicoController {
+
+    private final TopicoRepository topicoRepository;
+
+    public TopicoController(TopicoRepository topicoRepository) {
+        this.topicoRepository = topicoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<DatosRespuestaTopico> registrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico,
+                                                                UriComponentsBuilder uriComponentsBuilder) {
+        Topico topico = topicoRepository.save(new Topico(datosRegistroTopico));
+        DatosRespuestaTopico datosRespuestaTopico = new DatosRespuestaTopico(
+                topico.getId(),
+                topico.getTitulo(),
+                topico.getMensaje(),
+                topico.getFechaDeCreacion(),
+                topico.getStatus(),
+                topico.getAutor(),
+                topico.getCurso()
+        );
+
+        URI url = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(url).body(datosRespuestaTopico);
+    }
+}
