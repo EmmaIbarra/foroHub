c0c68e79459edc9d79bdab07f580e96a9338d1c3
Emma Ibarra <eibarram.77@gmail.com>
2025-01-05 17:46:41 -0300

    Creación record para actualizar datos

---
+++ src/main/java/com/aluracursos/foro_hub/domain/topico/DatosActualizarTopico.java
@@ -0,0 +1,8 @@
+package com.aluracursos.foro_hub.domain.topico;
+
+public record DatosActualizarTopico(
+        String titulo,
+        String mensaje,
+        String autor,
+        String curso) {
+}
