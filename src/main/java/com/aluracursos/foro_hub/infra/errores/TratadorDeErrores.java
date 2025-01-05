9ee74bb4924785615d76df8c53d4854af88af805
Emma Ibarra <eibarram.77@gmail.com>
2025-01-05 13:49:48 -0300

    tratado de error 404

--- src/main/java/com/aluracursos/foro_hub/infra/errores/TratadorDeErrores.java
+++ src/main/java/com/aluracursos/foro_hub/infra/errores/TratadorDeErrores.java
@@ -1,5 +1,7 @@
 package com.aluracursos.foro_hub.infra.errores;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,15 +10,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratadorDeErrores {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarError400(MethodArgumentNotValidException e){
         var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList() ;
         return ResponseEntity.badRequest().body(errores);
     }
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(EntityNotFoundException e){
+        var error = new DatosErrorInterno("Entidad no encontrada", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
     private record DatosErrorValidacion(String campo, String error){
         public DatosErrorValidacion(FieldError error) {
             this(error.getField(), error.getDefaultMessage());
         }
     }
+
+    private record DatosErrorInterno(String titulo, String detalle) {}
+
 }
