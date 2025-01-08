11dc2d8d1ca1a9811468988ae7d1afca55046364
Emma Ibarra <eibarram.77@gmail.com>
2025-01-07 22:08:31 -0300

    Creación clase Perfil

---
+++ src/main/java/com/aluracursos/foro_hub/domain/perfil/Perfil.java
@@ -0,0 +1,27 @@
+package com.aluracursos.foro_hub.domain.perfil;
+
+import com.aluracursos.foro_hub.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "perfiles")
+@Entity(name = "Perfil")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
