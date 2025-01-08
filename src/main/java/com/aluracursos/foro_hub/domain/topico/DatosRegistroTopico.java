f36b6c5dce7b87bdb157f0531dc4c31b9680efe4
Emma Ibarra <eibarram.77@gmail.com>
2025-01-08 15:57:23 -0300

    Eliminación imports obsoletos

--- src/main/java/com/aluracursos/foro_hub/domain/topico/DatosRegistroTopico.java
+++ src/main/java/com/aluracursos/foro_hub/domain/topico/DatosRegistroTopico.java
@@ -1,7 +1,6 @@
 package com.aluracursos.foro_hub.domain.topico;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record DatosRegistroTopico(
         @NotBlank
