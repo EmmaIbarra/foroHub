faab562f84c10b627f698c94600931a8f2b95265
Emma Ibarra <eibarram.77@gmail.com>
2025-01-08 12:04:06 -0300

    Creación servicio de autenticación

---
+++ src/main/java/com/aluracursos/foro_hub/infra/security/AutenticacionService.java
@@ -0,0 +1,22 @@
+package com.aluracursos.foro_hub.infra.security;
+
+import com.aluracursos.foro_hub.domain.usuario.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacionService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username);
+    }
+}
