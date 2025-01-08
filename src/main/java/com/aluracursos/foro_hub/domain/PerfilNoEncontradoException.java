a24c0f58d596871dc8e301e9dc35ce9e4b22f28d
Emma Ibarra <eibarram.77@gmail.com>
2025-01-08 16:32:58 -0300

    Excepción para perfil no encontrado

---
+++ src/main/java/com/aluracursos/foro_hub/domain/PerfilNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.aluracursos.foro_hub.domain;
+
+public class PerfilNoEncontradoException extends RuntimeException {
+    public PerfilNoEncontradoException(String message) {
+        super(message);
+    }
+}
