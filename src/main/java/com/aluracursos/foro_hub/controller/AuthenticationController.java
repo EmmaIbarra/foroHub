948b50297a9921a8ad82d19e40952ab8bdda9b41
Emma Ibarra <eibarram.77@gmail.com>
2025-01-08 15:54:18 -0300

    Creación controler de autenticación

---
+++ src/main/java/com/aluracursos/foro_hub/controller/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.aluracursos.foro_hub.controller;
+
+import com.aluracursos.foro_hub.domain.usuario.DatosAutenticacionUsuario;
+import com.aluracursos.foro_hub.domain.usuario.Usuario;
+import com.aluracursos.foro_hub.infra.security.DatosJWTToken;
+import com.aluracursos.foro_hub.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenService tokenService;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.email(),
+                datosAutenticacionUsuario.clave());
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
+    }
+}
