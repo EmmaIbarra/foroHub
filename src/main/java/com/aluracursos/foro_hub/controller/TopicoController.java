52fc2e61e2ba7d7a5d97b9462c48e550e5afd08c
Emma Ibarra <eibarram.77@gmail.com>
2025-01-08 15:53:23 -0300

    Eliminación de lo comentado

--- src/main/java/com/aluracursos/foro_hub/controller/TopicoController.java
+++ src/main/java/com/aluracursos/foro_hub/controller/TopicoController.java
@@ -3,14 +3,13 @@ package com.aluracursos.foro_hub.controller;
 import com.aluracursos.foro_hub.domain.TopicoNoEncontradoException;
 import com.aluracursos.foro_hub.domain.ValidacionException;
 import com.aluracursos.foro_hub.domain.topico.*;
-import com.aluracursos.foro_hub.infra.errores.TratadorDeErrores;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -55,16 +54,6 @@ public class TopicoController {
         return ResponseEntity.ok(topicoRepository.findAll(paginacion).map(DatosListadoTopico::new));
     }
 
-//    @GetMapping("/buscar")
-//    public ResponseEntity<List<DatosListadoTopico>> buscarTopicos(
-//            @RequestParam String curso,
-//            @RequestParam int anio) {
-//        LocalDateTime inicioAnio = LocalDateTime.of(anio, 1, 1, 0, 0);
-//        LocalDateTime finAnio = LocalDateTime.of(anio, 12, 31, 23, 59);
-//        List<Topico> topicos = topicoRepository.findByCursoAndFechaDeCreacionBetween(curso, inicioAnio, finAnio);
-//        return ResponseEntity.ok(topicos.stream().map(DatosListadoTopico::new).toList());
-//    }
-
     @GetMapping("/{id}")
     public ResponseEntity<DatosRespuestaTopico> retornaDatosTopico(@PathVariable Long id) {
         Optional<Topico> topicoOpt = topicoRepository.findById(id);
@@ -82,7 +71,6 @@ public class TopicoController {
             return ResponseEntity.ok(datosTopico);
         }
 
-        // Lanza una excepción si el tópico no se encuentra
         throw new TopicoNoEncontradoException("El tópico con ID " + id + " no fue encontrado.");
     }
 
