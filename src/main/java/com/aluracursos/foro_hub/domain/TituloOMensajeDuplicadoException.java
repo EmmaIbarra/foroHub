74576a42c0041f1d610a64353e2359dc1a00099c
Emma Ibarra <eibarram.77@gmail.com>
2025-01-09 17:02:13 -0300

    Creación de excepción para mensajes y titulos de tópicos duplicados

---
+++ src/main/java/com/aluracursos/foro_hub/domain/TituloOMensajeDuplicadoException.java
@@ -0,0 +1,7 @@
+package com.aluracursos.foro_hub.domain;
+
+public class TituloOMensajeDuplicadoException extends RuntimeException {
+    public TituloOMensajeDuplicadoException(String message) {
+        super(message);
+    }
+}
