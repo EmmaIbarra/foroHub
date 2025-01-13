b4589122d5be8703a1bd0983348ec54124696a0a
Emma Ibarra <eibarram.77@gmail.com>
2025-01-13 20:32:56 -0300

    Adición de id

--- src/main/java/com/aluracursos/foro_hub/domain/perfil/DatosPerfil.java
+++ src/main/java/com/aluracursos/foro_hub/domain/perfil/DatosPerfil.java
@@ -3,6 +3,7 @@ package com.aluracursos.foro_hub.domain.perfil;
 import jakarta.validation.constraints.NotBlank;
 
 public record DatosPerfil(
+        Long id,
         @NotBlank String nombre
 ) {
 }
