0a7eaacf44ea8d334502f0e1d94793cda18c58e8
Emma Ibarra <eibarram.77@gmail.com>
2025-01-13 17:45:34 -0300

    adición método para encontrar email exceptuando el del id

--- src/main/java/com/aluracursos/foro_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/aluracursos/foro_hub/domain/usuario/UsuarioRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     UserDetails findByEmail(String email);
     boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
